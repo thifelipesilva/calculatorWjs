2ec3d954f6702af7a2f3951542be2f06c6d4f371
unknown <thiagophsilva@hotmail.com>
2021-04-08 03:53:59 -0300

    Projeto de uma calculadora com JavaScript

---
+++ script/controller/CalcController.js
@@ -0,0 +1,15 @@
+class CalcController {
+
+    constructor() {
+        this._displayCalc = '0';
+        this._dataAtual;
+    }
+
+    get displayCalc() {
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor) {
+        this._displayCalc = valor;
+    }
+}
