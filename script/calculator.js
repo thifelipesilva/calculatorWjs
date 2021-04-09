e689e6c8d4870a683e9eef4b7cd60cc97c7c28ad
unknown <thiagophsilva@hotmail.com>
2021-04-09 02:48:58 -0300

    Incluindo metodos de evento nos botoes e exibição de data e hora

---
+++ script/calculator.js
@@ -0,0 +1,2 @@
+let calc = new CalcController();
+
--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,15 +1,101 @@
 class CalcController {
 
     constructor() {
-        this._displayCalc = '0';
-        this._dataAtual;
+
+        this._operation = [];
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+        this._currentDate;
+
+        this.initialize();
+        this.initButtonsEvents();
+    }
+
+    initialize() {
+
+        this.setDisplayDateTime();
+        setInterval(()=>{ //setInterval: função executada em um intervalo de tempo, de forma intermintente. O tempo é marcado em milisegundos.
+            this.setDisplayDateTime();
+        }, 1000);      
+
+    }
+
+    setDisplayDateTime() { //define exibição de data e hora.
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);//retorna uma string com a representação de parte da data baseando-se no idioma.
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);//retorna uma string com a representação da hora baseando-se no idioma.
+    }
+
+    
+
+    addEventListenerAll(element, events, fn) {  //Metodo criado para passar mais de um evento para o escutador de eventos.
+       
+        events.split(' ').forEach(event => { //Split tem a tarefa de pegar os eventos passados como strings ("click drag") e transformar em um array ['click', 'drag']. (" ") separa as strings pelo espaco.
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    execBtn(value) {
+        switch (value) {
+
+        }
+    }
+
+    initButtonsEvents() {
+
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+        buttons.forEach((button, index) => {
+
+            this.addEventListenerAll(button, 'click drag', e => { //Passando mais de um eventos.
+                let textBtn = button.className.baseVal.replace('btn-', "");//replace muda a exibição da classe. ex: ('btn-1' >>> '1')
+                this.execBtn(textBtn);
+            });
+
+            this.addEventListenerAll(button, 'mouseover mouseup mousedown', e => {
+                button.style.cursor = 'pointer';// Muda o estilo do cursor ao passar o mouse em cima do botão.
+            })
+
+        });     
+         
+                
+    }   
+    
+
+
+    //getters
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    get displayTime() {
+        return this._displayTime.innerHTML;
     }
 
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor) {
-        this._displayCalc = valor;
+    get currentDate() {
+        return new Date();
+    }
+
+    //setters
+
+    set displayDate(value) {
+        return this._dateEl.innerHTML = value;
+    }    
+
+    set displayTime(value) {
+        return this._timeEl.innerHTML = value;
+    }    
+
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
+    }   
+
+    set currentDate(value) {
+        this._currentDatec = value;
     }
 }
