685704522cf1a49cdaf204ed5381bcb750d5771f
unknown <thiagophsilva@hotmail.com>
2021-04-14 17:08:44 -0300

    Implentando a função igual e a função addDot

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor() {
 
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector('#display');
@@ -42,8 +44,12 @@ class CalcController {
 
 
     //Operações dos botões
-    clearAll() {
-        this._operation = []; // equivale a limpar os dados da tela
+    clearAll() { 
+
+        this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
+        this._operation = []; 
         this.setLastNumberToDisplay();
     }
 
@@ -56,7 +62,7 @@ class CalcController {
         return this._operation[this._operation.length - 1];
     }
 
-    setLastOperation(value) {
+    setLastOperation(value) { //o ultimo item do array recebe Value.
         this._operation[this._operation.length - 1] = value;
     }
 
@@ -72,20 +78,43 @@ class CalcController {
         }
     }
 
+   getResult() {
+        return eval(this._operation.join(''));// eval transforma td em operação e realiza o calculo. Join passa td pra string. É preferivel nesse caso usar join do que toString por causa do seu retorno.    
+
+   }
+
     calc() { //calcula a primeira operação, retorna o total da operação e o novo sinal.
        
-        let last = '';
+        let last = ''; 
+        this._lastOperator = this.getLastItem() //salva o sinal(_operation[3]) em _lastOperatos
 
-        if(this._operation.length > 3) { //verificação para executar os sinal de igual quando tiver mais q dois itens.
-            let last = this._operation.pop(); //salva o ultimo item no array na variavel Last. Sempre sera um sinal
+        //no caso de apertar o igual mais de uma vez com apenas 3 itens no array
+        if(this._operation.length < 3) { 
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
         }
-        
-        let result = eval(this._operation.join(''));// eval transforma td em operação e realiza o calculo. Join passa td pra string. É preferivel nesse caso usar join do que toString por causa do seu retorno.    
 
-        if(last == '%') { //Operação do sinal  porcento.
-            result = result / 100;
+        //esse if e else if representam as operações com sinal de igual.
+        if(this._operation.length > 3) { //verificação para salvar o sinal. No caso, se o array _operation tem mais de 3 itens o 4 é um sinal.
+            
+            last = this._operation.pop(); //O método pop() remove o último elemento de um array e retorna aquele elemento. Salva o quarto item, o sinal, na variavel Last.
+            this._lastNumber = this.getResult();
+
+        } else if(this._operation.length == 3) {
+
+            this._lastNumber = this.getLastItem(false);
+        }
+
+                
+        let result = this.getResult();
+
+        if(last == '%') { //Operação do sinal  porcento. Se last for % executa o if.
+            result /= 100;//result = result / 100;
             this._operation = [result]; //devolve um array com um elemento pq o '%' já é o quarto elemento.
-        } else {
+        
+        } else { // se não for "%", sera outro sinal
+
             this._operation = [result];    
             if (last) this._operation.push(last); //se last for diferente de vazio.
         }
@@ -93,15 +122,26 @@ class CalcController {
         this.setLastNumberToDisplay();
     }
 
-    setLastNumberToDisplay() { //atualiza o display.
-        let lastNumber;
+    getLastItem(isOperator = true) { // Pega o ultimo item.
+        let lastItem;
 
         for(let i = this._operation.length - 1; i >= 0; i--) { // sempre o array vai ter quatro itens, o quarto item sera um sinal.
-            if (!this.isOperator(this._operation[i])) { // se for um numero salva na variavel lastNumber.
-                lastNumber = this._operation[i];
+           
+            if(this.isOperator(this._operation[i]) == isOperator) { //se o ultimo item for um sinal, salva na variavel
+                lastItem = this._operation[i];
                 break;
             }
         }
+
+        if(!lastItem) {
+            lastItem = (isOperator) ? this._lastOperator : this.lastNumber;
+        }
+
+        return lastItem;
+    }
+
+    setLastNumberToDisplay() { //atualiza o display.
+        let lastNumber =  this.getLastItem(false);
 
         if (!lastNumber) lastNumber = 0; // Se lastNumber é vazio coloca 0; (para os casos ce e ac)
 
@@ -115,9 +155,6 @@ class CalcController {
             if(this.isOperator(value)){  
                 this.setLastOperation(value);
                
-            } else if(isNaN(value)){
-                console.log('c', value);
-
             } else { //primeiro numero a ser inserido na calculadora e primeiro numero depois do sinal de operação  vão ser inserido nesse else
                 this.pushOperation(value);
                 this.setLastNumberToDisplay();
@@ -130,7 +167,7 @@ class CalcController {
                 
             } else {
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
                 this.setLastNumberToDisplay();
                 
             }
@@ -139,6 +176,18 @@ class CalcController {
 
     setError() { //mensagem de erro
         this.displayCalc = 'Error';
+    }
+
+    addDot() {
+        let lastOperation = this.getLastOperation(); //pega a ultima operação
+        console.log(lastOperation)
+        if(this.isOperator(lastOperation) || !lastOperation) { // !lastOperation: se ele não existir.
+            this.pushOperation('0.');
+        } else{
+            this.setLastOperation(lastOperation.toString() + '.'); //
+        }
+
+        this.setLastNumberToDisplay();
     }
 
     execBtn(value) { //metodo da execução dos botoes.
@@ -177,7 +226,7 @@ class CalcController {
                 break;  
 
             case 'ponto':
-                this.addOperation('.');
+                this.addDot('.');
                 break;            
 
             case '0':
